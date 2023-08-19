f8ae2865168616fd61e6d9209a7c0271bf5a7145
giuseppegadola <giusgadola@gmail.com>
2023-08-19 17:12:45 +0200

    feat(eval): added basic evaluation function

---
+++ src/eval.rs
@@ -0,0 +1,14 @@
+use crate::{board::Board, consts::Colors};
+
+pub fn evaluate(b: &Board) -> i16 {
+    let white_mat = b.state.material[Colors::WHITE];
+    let black_mat = b.state.material[Colors::BLACK];
+
+    let eval = white_mat as i16 - black_mat as i16;
+
+    if b.state.active_color == Colors::BLACK {
+        -eval
+    } else {
+        eval
+    }
+}
