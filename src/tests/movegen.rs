e4908efed5184f630f8f38d807991ab42d6294fc
giuseppegadola <giusgadola@gmail.com>
2023-09-16 14:57:39 +0200

    chore(tests): added some more test positions

--- src/tests/movegen.rs
+++ src/tests/movegen.rs
@@ -7,6 +7,8 @@ use crate::{
     moves::{defs::MoveType, MoveGenerator},
 };
 
+// positions and results are from https://www.chessprogramming.org/Perft_Results
+
 struct DbgRefs<'a> {
     timer: Instant,
     board: &'a mut Board,
@@ -49,7 +51,7 @@ fn search(refs: &mut DbgRefs, depth: u8) {
 
 // this function sets up the given fen string, searches to the given depth and panics
 // if the number of legal moves found is different from the argument one
-fn do_test(fen: &str, depth: u8, moves: u64, captures: u64) {
+fn do_test(fen: &str, depth: u8, moves: u64, captures: Option<u64>) {
     let mut b = Board::new();
     let mg = MoveGenerator::new();
     b.read_fen(fen).unwrap();
@@ -66,7 +68,9 @@ fn do_test(fen: &str, depth: u8, moves: u64, captures: u64) {
         refs.timer.elapsed().as_millis()
     );
     assert_eq!(refs.legal_moves, moves);
-    assert_eq!(refs.captures, captures);
+    if let Some(captures) = captures {
+        assert_eq!(refs.captures, captures);
+    }
 }
 
 #[test]
@@ -80,7 +84,7 @@ fn startpos() {
     ];
     let time = Instant::now();
     for (depth, moves, captures) in startpos {
-        do_test(START_FEN, depth, moves, captures)
+        do_test(START_FEN, depth, moves, Some(captures))
     }
     println!("Startpos time: {}ms", time.elapsed().as_millis());
 }
@@ -101,8 +105,22 @@ fn kiwipete() {
             "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
             depth,
             moves,
-            captures,
+            Some(captures),
         )
     }
     println!("Kiwipete time: {}ms", time.elapsed().as_millis());
+}
+
+#[test]
+fn other_positions() {
+    let time = Instant::now();
+    let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+    do_test(fen, 3, 62379, None);
+    do_test(fen, 4, 2103487, None);
+
+    let fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ";
+    do_test(fen, 3, 89890, None);
+    do_test(fen, 4, 3894594, None);
+
+    println!("Other positions time: {}ms", time.elapsed().as_millis())
 }
