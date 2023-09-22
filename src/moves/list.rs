4f20891e3ca6f6f1936326e52581eac17dd3187d
giusgad <giusgadola@gmail.com>
2023-09-22 11:49:36 +0200

    feat(search): tt first move ordering

--- src/moves/list.rs
+++ src/moves/list.rs
@@ -19,13 +19,23 @@ impl MoveList {
         self.index += 1;
     }
 
+    fn swap(&mut self, i: usize, j: usize) {
+        (self.list[i], self.list[j]) = (self.list[j], self.list[i]);
+    }
+
     // reorder the moves for a faster search
-    pub fn reorder(&mut self) {
-        let mut top = 0;
-        for i in 0..self.index {
+    pub fn reorder(&mut self, tt_move: Option<Move>) {
+        // if a move retrieved from the tt is available it will be repositioned to the top
+        // so we leave a space for it at the beginning of the list
+        let mut top = if tt_move.is_some() { 1 } else { 0 };
+
+        #[allow(clippy::mut_range_bound)]
+        for i in top..self.index {
             let m = &self.list[i];
-            if m.move_type() == MoveType::Capture {
-                (self.list[top], self.list[i]) = (self.list[i], self.list[top]);
+            if tt_move.is_some_and(|tt_m| &tt_m == m) {
+                self.swap(0, i);
+            } else if m.move_type() == MoveType::Capture {
+                self.swap(top, i);
                 top += 1;
             }
         }
--- src/search/alpha_beta.rs
+++ src/search/alpha_beta.rs
@@ -39,12 +39,15 @@ impl Search {
         refs.info.nodes += 1;
 
         // only try to load from the tt if it's not the first move
+        let mut tt_move = None;
         if refs.info.ply != 0 {
             let mut tt_eval = None;
             // try to get value from the transposition table
             if let Some(data) = refs.tt.get(refs.board.state.zobrist_hash) {
                 // TODO: tt first move ordering;
-                (tt_eval, _) = data.get_values(alpha, beta, depth);
+                let (eval, m) = data.get_values(alpha, beta, depth);
+                tt_eval = eval;
+                tt_move = Some(m);
             }
             if let Some(eval) = tt_eval {
                 refs.tt_loads += 1;
@@ -59,7 +62,7 @@ impl Search {
         let mut best_move = Move { data: 0 };
 
         let mut moves = refs.mg.get_all_legal_moves(refs.board, false);
-        moves.reorder();
+        moves.reorder(tt_move);
 
         for &m in moves.iter() {
             let legal = refs.board.make_move(m, refs.mg);
